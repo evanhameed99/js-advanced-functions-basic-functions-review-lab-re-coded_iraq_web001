d7a633987cb6ee86b03cb05d1d7c8253ab7bee3f
Evan hameed <evanhameed9966@gmail.com>
2020-06-07 20:34:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,58 +1,41 @@
 // Your code here
-
-function saturdayFun(activity = "roller-skate"){
-
-return `This Saturday, I want to ${activity}!` ;
-
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`
 }
-saturdayFun();
-saturdayFun('bathe my dog')
 
-let mondayWork = function (activity = 'go to the office'){
+function mondayWork(activity="go to the office") {
   return `This Monday, I will ${activity}.`
 }
-mondayWork();
-mondayWork('work from home');
 
-function wrapAdjective(wrapper = '*'){
-
-  return function (adjective = 'special'){
-    return `You are ${wrapper}${adjective}${wrapper}!`
+function wrapAdjective(visualFlair="*") {
+  return function (adjective='special') {
+    return `You are ${visualFlair}${adjective}${visualFlair}!`
   }
 }
-wrapAdjective()('a hard worker');
-wrapAdjective('||')('dedicated programmer');
 
-let Calculator= {
-  add: function(a,b){
-    return a+b;
+let Calculator = {
+  add: function () {
+    return 1 + 3
   },
-  subtract: function(a,b){
-    return a-b;
+  subtract: function () {
+    return 1 - 3
   },
-  multiply: function(a,b){
-    return a*b;
+  multiply: function () {
+    return 1 * 3
   },
-  divide: function(a,b){
-    return a/b;
+  divide: function () {
+    return 10 / 5
   }
-
 }
-Calculator.add(1,3);
-Calculator.subtract(1,3);
-Calculator.multiply(1,3);
-Calculator.divide(10,5);
 
-
-function actionApplyer(startingInt , arr){
-if(arr.length ===0){
-  return startingInt;
-}
-else{
-  arr= [mult,add,mod];
-  stratingInt = mult(startingInt);
-  stratingInt = add(startingInt);
-  stratingInt = add(startingInt);
-  return startingInt;
-}
+function actionApplyer(startingInteger, arrOfFns) {
+  if (!arrOfFns.length){
+    return startingInteger
+  } else {
+    let [mult,add,mod] = arrOfFns;
+    startingInteger = mult(startingInteger)
+    startingInteger = add(startingInteger)
+    startingInteger = mod(startingInteger)
+    return startingInteger
+  }
 }
