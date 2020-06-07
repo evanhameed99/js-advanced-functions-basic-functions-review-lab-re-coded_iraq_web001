0cc34e74835b3e66998a784dce41dae88df7bb4b
Evan hameed <evanhameed9966@gmail.com>
2020-06-07 19:33:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ return `This Saturday, I want to ${activity}!` ;
 }
 saturdayFun();
 saturdayFun('bathe my dog')
+
+let mondayWork = function (activity = 'go to the office'){
+  return `This Monday, I will ${activity} `
+}
